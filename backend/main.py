da340216643a3e3068545a81a056e30de9315abd
geromansur0-a11y <geromansur0@gmail.com>
2025-12-12 15:56:33 +0700

    Update main.py
    Signed-off-by: geromansur0-a11y <geromansur0@gmail.com>

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,6 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 import PyPDF2
-from docx import Document
 from openpyxl import load_workbook
 import httpx
 from telegram import Bot
